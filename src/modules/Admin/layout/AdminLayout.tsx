740cea97dc805a0d40dedabd21e520f6d0f647b4
JavierAQR <nonamesteam123@gmail.com>
2025-12-07 22:00:19 -0500

    feat(frontend): Mejorados estilos de servicios y productos

--- src/modules/Admin/layout/AdminLayout.tsx
+++ src/modules/Admin/layout/AdminLayout.tsx
@@ -1,33 +1,37 @@
-import { useEffect, type ReactNode } from "react";
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
 import { useAuthStore } from "../../../stores/authStore";
 import AdminSidebar from "../components/AdminSidebar";
 
 interface AdminLayoutProps {
-  children?: ReactNode;
+  children?: React.ReactNode;
 }
 
 const AdminLayout = ({ children }: AdminLayoutProps) => {
   const { user, isAuthenticated, hydrateFromStorage } = useAuthStore();
-  const location = useLocation();
+  // const location = useLocation();
 
   useEffect(() => {
     hydrateFromStorage();
   }, [hydrateFromStorage]);
+  
 
   const isAdmin = isAuthenticated && user?.role === "ADMIN";
+  console.log(isAdmin);
+  
 
-  if (!isAdmin) {
-    // redirige si no es admin
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
+  // if (!isAdmin) {
+  //   return <Navigate to="/login" state={{ from: location }} replace />;
+  // }
 
   return (
-    <div className="min-h-screen flex bg-gray-50">
+    <div className="min-h-screen flex bg-linear-to-br from-gray-50 to-gray-100">
       <AdminSidebar />
 
-      <main className="flex-1 p-4 md:p-6 lg:p-8">
-        {children ?? <Outlet />}
+      <main className="flex-1 p-4 md:p-6 lg:p-8 overflow-auto">
+        <div className="max-w-7xl mx-auto">
+          {children ?? <Outlet />}
+        </div>
       </main>
     </div>
   );
